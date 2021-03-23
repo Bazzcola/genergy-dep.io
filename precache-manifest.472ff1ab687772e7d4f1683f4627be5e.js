a63b497009afb5ba6e1433de2410e3d60389930b
Bazzcola <60352726+Bazzcola@users.noreply.github.com>
2021-03-23 22:25:57 +0200

    Add files via upload

---
+++ precache-manifest.472ff1ab687772e7d4f1683f4627be5e.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "26acd1eb9c3ceb69939127fe343b1420",
+    "url": "/index.html"
+  },
+  {
+    "revision": "dbe06d2d2adbbee91e19",
+    "url": "/static/css/2.84bd7022.chunk.css"
+  },
+  {
+    "revision": "364554644dd7ddbc32d0",
+    "url": "/static/css/main.531eb7d1.chunk.css"
+  },
+  {
+    "revision": "dbe06d2d2adbbee91e19",
+    "url": "/static/js/2.d6f096a8.chunk.js"
+  },
+  {
+    "revision": "4c7326d1825e209363fe49c463938b53",
+    "url": "/static/js/2.d6f096a8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "364554644dd7ddbc32d0",
+    "url": "/static/js/main.c7267436.chunk.js"
+  },
+  {
+    "revision": "8842da0ec4d13bbe3f04",
+    "url": "/static/js/runtime-main.a8a9ac01.js"
+  }
+]);
